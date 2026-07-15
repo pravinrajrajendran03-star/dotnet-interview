aceb87928741c26b7d1f27c69bc6cc8531c3ffef
HP <pravinrajrajendran03@gmail.com>
2026-07-15 20:56:02 +0530

    coomit: Adding interface for service class

---
+++ TodoApi/Services/ITodoService.cs
@@ -0,0 +1,12 @@
+﻿using TodoApi.Models
+
+namespace TodoApi.Services
+{
+    public interface ITodoService
+    {
+        Todo CreateTodo(CreateTodoRequest request);
+        List<TodoApi> GetAllTodos();
+        Todo? UpdateTodo(int id, UpdateTodoRequest request);
+        bool DeleteTodo(int id);
+    }
+}
